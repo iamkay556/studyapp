00d87af1281744c1295fee8a58797410a57c2367
kay singhal <130114612+iamkay556@users.noreply.github.com>
2024-11-24 23:50:30 -0800

    revert

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+import { doSignOut } from '../auth';
+import { auth } from '../firebase';
 
-const Nav = () => {
+const Header = () => {
+    const navigate = useNavigate();
+    const { userLoggedIn } = useAuth();
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    console.log(userLoggedIn);
+
+    const handleLogout = async () => {
+        await doSignOut(auth);
+        navigate('/login');
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true); // Enable blur effect
+            } else {
+                setIsScrolled(false); // Remove blur effect
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div>Nav</div>
-    )  
+        <div>
+            <header className={`headernav ${isScrolled ? 'nav-blur' : ''}`}>
+                {userLoggedIn ? (
+                    <nav className="headernav">
+                        <div>
+                            <img src="polygen.png" alt="Logo" className="logoimg" />
+                        </div>
+                        <button className="navlink-logout" onClick={handleLogout}>
+                            Logout
+                        </button>
+                    </nav>
+                ) : (
+                    <nav className="headernav">
+                        <div>
+                            <img src="polygen.png" alt="Logo" className="logoimg" />
+                        </div>
+                    </nav>
+                )}
+            </header>
+        </div>
+    );
 };
 
-export default Nav;
+export default Header;
